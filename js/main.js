7dbe565dac612842d75ba49e97b3f800ad8c0447
xtarx <xtarx0@gmail.com>
2015-08-16 19:33:10 +0200

    no message

--- js/main.js
+++ js/main.js
@@ -1,16 +1,10 @@
+
 // Initialize endpoint
-$('#start').on('click', function () {
-    // First, grab the SAT token from the server
-    var token = "eyJjdHkiOiAidHdpbGlvLXNhdDt2PTEiLCAiYWxnIjogIkhTMjU2IiwgInR5cCI6ICJKV1QifQ.eyJleHAiOiAxNDM5NDc3MDc2LCAiZ3JhbnRzIjogW3siYWN0IjogWyJsaXN0ZW4iLCAiaW52aXRlIl0sICJyZXMiOiAic2lwOnFAQUM3NmMzZDVkMjQwODJlNzNkNDE5ZjEzODRjNDliNjU2NC5lbmRwb2ludC50d2lsaW8uY29tIn0sIHsiYWN0IjogWyJQT1NUIl0sICJyZXMiOiAiaHR0cHM6Ly9hcGkudHdpbGlvLmNvbS8yMDEwLTA0LTAxL0FjY291bnRzL0FDNzZjM2Q1ZDI0MDgyZTczZDQxOWYxMzg0YzQ5YjY1NjQvVG9rZW5zLmpzb24ifV0sICJzdWIiOiAiQUM3NmMzZDVkMjQwODJlNzNkNDE5ZjEzODRjNDliNjU2NCIsICJuYmYiOiAxNDM5NDczNDc2LCAiaXNzIjogIlNLNmQxMTVhZjNhNjg0NGIzNDdhY2E5ODI3MmViZDAwOWIiLCAianRpIjogIlNLNmQxMTVhZjNhNjg0NGIzNDdhY2E5ODI3MmViZDAwOWItMTQzOTQ3MzQ3NiJ9.VUGpGvLoVTHBhxgaSC-DdGcPXSxZvPCT3d8-N4wroIA";
+function start() {
+       // First, grab the SAT token from the server
+    var token = "";
     // Create the endpoint, and then initialize the main calling app
     var endpoint = new Twilio.Endpoint(token);
-    // $('#startDiv').hide();
-    $('#callDiv').show();
-    init(endpoint);
-
-});
-
-function start() {
 
 }
 // Initialize video calling app with my endpoint
@@ -25,14 +19,6 @@ function init(endpoint) {
 
     });
 
-    // Start an outbound conversation
-    $('#call').on('click', function () {
-        endpoint.createConversation($('#other-name').val())
-        .then(showConversation);
-    });
-
-    // Listen for incoming calls
-    //endpoint.listen();
 
     // Listen for incoming calls
     endpoint.listen().then(function (endpoint) {
@@ -52,3 +38,5 @@ function showConversation(conversation) {
         participant.media.attach('#you');
     });
 }
+
+start();
