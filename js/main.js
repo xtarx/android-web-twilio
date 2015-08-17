e1d2bb394d206a0f9720c49c98acdc110ae62791
xtarx <xtarx0@gmail.com>
2015-08-17 13:05:09 +0200

    init(endpoint)

--- js/main.js
+++ js/main.js
@@ -8,12 +8,13 @@ function getParameterByName(name) {
 // Initialize endpoint
 function start() {
        // First, grab the SAT token from the server
-//    var token = "";
+   // var token = "eyJhbGciOiAiSFMyNTYiLCAiY3R5IjogInR3aWxpby1zYXQ7dj0xIiwgInR5cCI6ICJKV1QifQ.eyJuYmYiOiAxNDM5ODA4ODQ4LCAiaXNzIjogIlNLNmQxMTVhZjNhNjg0NGIzNDdhY2E5ODI3MmViZDAwOWIiLCAiZ3JhbnRzIjogW3sicmVzIjogInNpcDpxQEFDNzZjM2Q1ZDI0MDgyZTczZDQxOWYxMzg0YzQ5YjY1NjQuZW5kcG9pbnQudHdpbGlvLmNvbSIsICJhY3QiOiBbImxpc3RlbiIsICJpbnZpdGUiXX0sIHsicmVzIjogImh0dHBzOi8vYXBpLnR3aWxpby5jb20vMjAxMC0wNC0wMS9BY2NvdW50cy9BQzc2YzNkNWQyNDA4MmU3M2Q0MTlmMTM4NGM0OWI2NTY0L1Rva2Vucy5qc29uIiwgImFjdCI6IFsiUE9TVCJdfV0sICJleHAiOiAxNDM5ODEyNDQ4LCAianRpIjogIlNLNmQxMTVhZjNhNjg0NGIzNDdhY2E5ODI3MmViZDAwOWItMTQzOTgwODg0OCIsICJzdWIiOiAiQUM3NmMzZDVkMjQwODJlNzNkNDE5ZjEzODRjNDliNjU2NCJ9.vSL2fqjmYP8Q6QmFLNqM26p19hKESTy3aenIs0hH2qQ";
     
     var token=getParameterByName('token');
-    console.log(token);
+    // console.log(token);
     // Create the endpoint, and then initialize the main calling app
     var endpoint = new Twilio.Endpoint(token);
+    init(endpoint);
 
 }
 // Initialize video calling app with my endpoint
@@ -23,7 +24,7 @@ function init(endpoint) {
     // Automatically accept any incoming calls
 
     endpoint.on('invite', function (invitation) {
-        console.log('Received an Invite to join a Conversation from ' + invitation.from);
+        console.log('Received an Invite to <join></join> a Conversation from ' + invitation.from);
         invitation.accept().then(showConversation);
 
     });
